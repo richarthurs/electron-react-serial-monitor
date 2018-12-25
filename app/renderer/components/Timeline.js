2e33ffa52ca39aa9be78e1865dee622d9eed0c76
richarthurs <arthursrich@gmail.com>
2018-12-24 22:47:01 -0800

    Added style for commands
    - commands are now highlighted with a different colour in the timeline
    - refactored timeline component to be a class

--- app/renderer/components/Timeline.js
+++ app/renderer/components/Timeline.js
@@ -5,40 +5,63 @@ import ListGroup from 'react-bootstrap/lib/ListGroup'
 import Row from 'react-bootstrap/lib/Row'
 import Col from 'react-bootstrap/lib/Col'
 
+/* Timeline component
+https://hackernoon.com/how-to-redux-with-react-836ed6d85330
+https://reactjs.org/docs/faq-styling.html
+https://stackoverflow.com/questions/28567549/how-to-use-if-within-a-map-return/28567644
+*/
+class Timeline extends Component{
+    render(){
+        // Create the list of items
+
+        const timelineItems = this.props.timelinedata.map(function (timelinedata,i){
+            let className = "";
+
+            if (timelinedata.data_type == "COMMAND"){
+                className += ' command';
+                console.log("cmd");
+            }
+            return(
+                <ListGroup.Item key={timelinedata.timeline_count} className={className}>
+
+                <Row className="show-grid">
+                <Col xs={12} md={8}>
+                    <Badge pill variant="primary">{timelinedata.epoch_received}</Badge>
+                    {timelinedata.text} 
+                </Col>
+                <Col xs={6} md={4}>
+                    <Badge pill variant="success">{timelinedata.data_type}</Badge>
+                </Col>
+                </Row>
+                </ListGroup.Item>  
+            )
+        }.bind(this));
+                        
+    // Return the layout
+    return(
+        <div>
+        <ListGroup>
+            {timelineItems}
+        </ListGroup>
+        </div>
+    )}
+
+}
+
+
 /* Map State to Props
     - assign variables from state (the redux store) to this component's props
         - allows state information to be used in the layout
 */
-const mapStateToProps = state => {
-    return { timelinedata: state.timelinedata};
-  };
 
+const mapStateToProps = state => ({
+    timelinedata: state.timelinedata,
+});
 
-/* OBCDataList component
-    - obcdata prop gets information from the redux store
-    - loop through the array, pull out fields, create list items
-*/
-const OBCDataList = ({ timelinedata }) => (
-    <div>
-    <ListGroup>
-        {timelinedata.map(timelinedata => (
-            <ListGroup.Item key={timelinedata.timeline_count}>
-
-            <Row className="show-grid">
-            <Col xs={12} md={8}>
-                <Badge pill variant="primary">{timelinedata.epoch_received}</Badge>
-                {timelinedata.text} 
-            </Col>
-            <Col xs={6} md={4}>
-                <Badge pill variant="success">{timelinedata.data_type}</Badge>
-            </Col>
-            </Row>
-            </ListGroup.Item>
-        ))}
-    </ListGroup>
-    </div>
-);
+const mapDispatch = dispatch => ({});
 
 /* Magic to hook up the state to the props */
-const Timeline = connect(mapStateToProps)(OBCDataList);
-export default Timeline;  /* the name of this component */
+export default connect(
+    mapStateToProps,
+    mapDispatch
+)(Timeline);
